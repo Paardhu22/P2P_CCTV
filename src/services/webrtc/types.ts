94c814f7401ed6f8b7f53fec056846bcfd8bf364
Paardhu22 <paardhivreddy22@gmail.com>
2026-07-21 00:24:44 +0530

    update src/services/webrtc/types.ts

--- src/services/webrtc/types.ts
+++ src/services/webrtc/types.ts
@@ -1,7 +1,10 @@
+import { MediaStream } from 'react-native-webrtc';
+
 export type WebRTCPeerState = 'Disconnected' | 'Connecting' | 'Connected' | 'Failed' | 'Closed';
 
 export interface PeerStateInfo {
   connectionState: WebRTCPeerState;
   signalingState: string;
   iceConnectionState: string;
+  remoteStream: MediaStream | null;
 }
